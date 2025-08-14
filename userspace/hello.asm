efd23cc3eac2d417c0fd892a0747643270b15931
argaldo <argaldo@gmail.com>
2025-08-14 12:36:43 +0200

    two syscalls test

--- userspace/hello.asm
+++ userspace/hello.asm
@@ -4,7 +4,10 @@ section .text
    global _start
 
 
+
 _start:
    mov rax, 1        ; syscall id 1 (example: print, exit, etc.)
+   int 0x80
+   mov rax, 2        ; syscall id 2 (another syscall)
    int 0x80
    ret
